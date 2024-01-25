2d184be9202b74ecea8aaeea5f64b1b0879595ea
WambiruL <wambirulorraine@gmail.com>
2024-01-25 12:17:10 +0300

    Add the header section

--- public_pulse/settings.py
+++ public_pulse/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'landing_page.middleware.DisableCacheMiddleware'
 ]
 
 ROOT_URLCONF = 'public_pulse.urls'
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/Images')
+MEDIA_URL = '/Images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
